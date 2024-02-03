bc55924f6be1176544d4e2c60094509a67bca9b9
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-03 18:18:22 +0100

    Update App.js

--- App.js
+++ App.js
@@ -13,17 +13,14 @@ export default function App() {
         initialRouteName="Home"
         screenOptions={{
           headerStyle: { backgroundColor: "#6a51ae" },
-          headerTintColor: "#fff",
           headerTitleStyle: { fontWeight: "bold" },
-          //headerRight doesn't display for some reason
-          headerRight: () => {
+          headerTintColor: "#fff",
+          contentStyle: { backgroundColor: "#e8e4f3" },
+          headerRight: () => (
             <Pressable onPress={() => alert("Menu button pressed!")}>
               <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
-            </Pressable>;
-          },
-          contentStyle: {
-            backgroundColor: "#e8e4f3",
-          },
+            </Pressable>
+          ),
         }}
       >
         <Stack.Screen
